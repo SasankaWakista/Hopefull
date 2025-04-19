4a205f4b6db4698009b7b14143a4999e7275168b
Sasanka Wakista <140165077+SasankaWakista@users.noreply.github.com>
2025-04-19 14:40:15 +0530

    Delete Admin_Dashboard directory

--- Admin_Dashboard/head.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="css/header.css">
-<link rel="stylesheet" href="css/logout.css">
-<header><nav class="navbar">
-    <div class="logo">
-        <img src="images/logo.png" alt="Hopefull Logo">
-    </div>
-    <h1>Welcome to the HopeFull Administrator Dashboard!</h1>
-  </header>
